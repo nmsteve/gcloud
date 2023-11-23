4a101362d82c902add79a2841d9e7b1a1d099102
nmsteve <nmsteve45@gmail.com>
2023-11-23 14:00:35 +0300

    modification to print mesage

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ app.get('/', (req, res) => {
 
 const port = parseInt(process.env.PORT) || 8080;
 app.listen(port, () => {
-    console.log(`helloworld: listening on port ${port}`);
+    console.log(`Google API Hosting Test: listening on port ${port}`);
 });
